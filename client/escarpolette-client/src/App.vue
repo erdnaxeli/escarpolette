a9485686c32b2744eab725e85c01f9f567b39462
cdann <cdannapp@student.42.fr>
2018-12-09 01:14:20 +0100

    first install vuejs and vuetify

---
+++ client/escarpolette-client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <v-avatar
+            color="grey lighten-4"
+          >
+            <img src="./assets/logo.png" alt="avatar">
+          </v-avatar>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
