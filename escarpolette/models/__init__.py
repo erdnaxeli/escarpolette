5fa50d9b0d4a574f89f089a72cf46dde74c8fa08
Alexandre Morignot <amorignot@meilleursagents.com>
2019-12-13 18:12:33 +0100

    New model Playlist

--- escarpolette/models/__init__.py
+++ escarpolette/models/__init__.py
@@ -1,1 +1,2 @@
-from .item import Item
+from escarpolette.models.item import Item
+from escarpolette.models.playlist import Playlist
--- escarpolette/models/item.py
+++ escarpolette/models/item.py
@@ -1,21 +1,21 @@
-from escarpolette.extensions import db
-from datetime import datetime
 from sqlalchemy import text
 
+from escarpolette.extensions import db
+from escarpolette.models.base_model_mixin import BaseModelMixin
 
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(
-        db.DateTime,
-        default=datetime.now,
-        server_default=text("datetime()"),
-        index=True,
-        nullable=False,
-    )
 
+class Item(BaseModelMixin, db.Model):
     artist = db.Column(db.String(255))
     duration = db.Column(db.Integer)
     played = db.Column(db.Boolean, default=False, server_default=text("FALSE"))
     title = db.Column(db.String(255))
     url = db.Column(db.String(255), unique=True)
     user_id = db.Column(db.String(36), index=True, nullable=False)
+    playlist_id = db.Column(
+        db.Integer,
+        db.ForeignKey("playlist.id", ondelete="CASCADE"),
+        index=True,
+        nullable=False,
+    )
+
+    playlist = db.relationship("Playlist", back_populates="items",)
