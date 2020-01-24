356984d0bbe610a90576ce5ca12ee936746577f4
Alexandre Morignot <amorignot@meilleursagents.com>
2020-01-24 14:58:19 +0100

    fixup! fixup! WIP fastapi

--- escarpolette/settings.py
+++ escarpolette/settings.py
@@ -1,17 +1,26 @@
 from datetime import timedelta
-
-from flask import Flask
+from configparser import ConfigParser
+from typing import TextIO
 
 
 class Default:
     # Server
     HOST = "127.0.0.1"
+    PORT = 8000
 
     # Database
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DATABASE_URI = "sqlite:///"
 
     # Authentication
     REMEMBER_COOKIE_DURATION = timedelta(days=390)  # ~13 months
 
-    def __init__(self, app: Flask):
+
+class Config(Default):
+    def __init__(self, file: TextIO):
+        config = ConfigParser().read_file(file)
+
+        self.HOST = config.get("HOST", self.HOST)
+        self.DATABASE_URI = config.get("DATABASE_URI", self.DATABASE_URI)
+
         self.SQLALCHEMY_DATABASE_URI = f"sqlite:///{app.instance_path}/db.sqlite"
